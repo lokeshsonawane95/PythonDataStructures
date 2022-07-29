786463a1d75631b271f52f907df91d00cf067d7f
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-07-29 16:06:47 +0530

    Added to DataStructures

---
+++ sets.py
@@ -0,0 +1,22 @@
+def sets():
+    """
+    Function implements set
+    :return: operation performed on set
+    """
+    try:
+        a = {2, 3, 5, 7}
+        b = {1, 3, 5}
+        print("Set a is : ", a)
+        print("Set b is : ", b)
+        print("a | b : ", a | b)
+        print("a & b : ", a & b)
+        print("a - b : ", a - b)
+        print("b - a : ", b - a)
+        print("a ^ b : ", a ^ b)
+    except Exception as err:
+        print(err)
+
+
+if __name__ == "__main__":
+    print("Set operations are")
+    sets()
